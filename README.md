59ea3845d5234d3c94afab6e088218eca73ff9c6
JinLee794 <94473824+JinLee794@users.noreply.github.com>
2022-08-02 12:53:02 -0500

    Added readme, cleanup

---
+++ README.md
@@ -0,0 +1,82 @@
+# ADO Rest Sandbox
+
+This is a demo sandbox to explore API-related topics to assist in Azure DevOps resource management.
+
+Inside this project you will find various Powershell scripts located in [./Scripts/](./Scripts/) directory, and accessory tools that assist in development efforts within the [./Tools/](./Tools/) directory.
+
+## Topics
+
+1. [Managing Project Agent-Pool Permissions](#managing-project-agent-pool-permissions)
+
+## Managing Project Agent-Pool Permissions
+
+### Problem
+
+Currently in the Azure DevOps portal, the capability to modify default behaviors for agent pool permission allocation on new projects is not supported. This can raise concerns for many as the default behavior is to grant **administrative** access to project-scoped roles (e.g Build/Release Administrator, Project Administrator) to organization-scoped agent pool resources.
+Meaning, project-scoped users can potentially remove existing agents that are being leveraged by other projects within the organization, and add their own agents that could bypass any security or standards that are baked into dedicated CICD machine images.
+
+### Solution
+
+Through leveraging the Azure DevOps REST API, facilitate the project creation as well as the agent pool role assignments for all default project permissions.
+
+#### Relevant Files
+
+1. [PostmanCollection](./Tools/PostmanCollections/CreateProject-AgentPoolConfig.ADO%20Rest%20API.postman_collection.json)
+1. [CreateProject-AgentPoolConfig.ps1](./Scripts/CreateProject-AgentPoolConfig.ps1)
+
+#### Steps to Deploy
+
+##### [CreateProject-AgentPoolConfig.ps1](./Scripts/CreateProject-AgentPoolConfig.ps1)
+
+1. [Generate a DevOps PAT](https://docs.microsoft.com/en-us/azure/devops/organizations/accounts/use-personal-access-tokens-to-authenticate?view=azure-devops&tabs=Windows#create-a-pat)
+    1. TODO: Narrow down the exact permissions required to make these requests
+    1. Current setup (working):
+        // TODO: Narrow down exact permissions needed
+        **_ !!! The permissions below are most likely more permissive than necessary. Use at your own risk !!! _**
+        1. Agent Pools: Read & Manage
+        1. Project and Team: Read, write, & manage
+        1. Security: Manage
+        1. Tokens: Read & manage
+        1. User Profile
+
+1. Set the Azure DevOps PAT to the `$env:PAT` environment variable
+
+```pwsh
+# Set PAT as environment var
+$env:PAT = '<your devops pat>'
+```
+
+1. Invoke the script with your project variables
+    Input Arguments:
+    - `$Organization`: URI of your DevOps organization (e.g `https://dev.azure.com/my-ado-org`)
+    - `$ProjectName`: Name of the new project to create, or an existing project to update the project roles on.
+    - `$ProjectDescription`: [OPTIONAL] Short project description to initialize with (defaults to `$null`).
+    - `$VersionControl`: [OPTIONAL] Version control to be used by the repositories inside the project, can be `TFVC` or `Git` (defaults to `Git`).
+    - `ProcessTemplate`: [OPTIONAL] Process template name to search for within the organization to initialize the project with (defaults to `Scrum`).
+    - `PoolDefaultRole`: [OPTIONAL] Agent pool permission to assign the default project permissions upon creation (defaults to `User`).
+
+```pwsh
+./Scripts/CreateProject-AgentPoolConfig.ps1 `
+  -Organization "https://dev.azure.com/some-org" `
+  -ProjectName "My-New-Project" `
+  -ProjectDescription "Project and Agent Pool permission management via REST API PoC"
+```
+
+##### [PostmanCollection](./Tools/PostmanCollections/CreateProject-AgentPoolConfig.json)
+
+1. [Generate a DevOps PAT](https://docs.microsoft.com/en-us/azure/devops/organizations/accounts/use-personal-access-tokens-to-authenticate?view=azure-devops&tabs=Windows#create-a-pat)
+    1. TODO: Narrow down the exact permissions required to make these requests
+    1. Current setup (working):
+        // TODO: Narrow down exact permissions needed
+        **_ !!! The permissions below are most likely more permissive than necessary. Use at your own risk !!! _**
+        1. Agent Pools: Read & Manage
+        1. Project and Team: Read, write, & manage
+        1. Security: Manage
+        1. Tokens: Read & manage
+        1. User Profile
+1. Follow the [PostMan import documentation](https://learning.postman.com/docs/getting-started/importing-and-exporting-data/#importing-data-into-postman) to import the `./Tools/PostmanCollections/CreateProject-AgentPoolConfig.json` collection
+1. Set up your variables within your environment to execute the templated queries.
+    - organization
+    - projectId
+    - PAT (set as secret!!!)
+    - roleDisplayName
--- Scripts/CreateProject-AgentPoolConfig.ps1
+++ Scripts/CreateProject-AgentPoolConfig.ps1
@@ -2,35 +2,42 @@
 # Custom Project Creation
 # --------------------------------------------------
 # Goal: Facilitate Project Creation and Update Agent
-#         Pool Permissions for the Project Roles
+#         Pool Permissions on the default Project permissions
+#         for both existing and new Projects
 # --------------------------------------------------
-# 1. Retrieve Process Template ID by Name
-# 2. Create Project with said Process Template ID
+# 1. Retrieve Process Template ID by Name $ProcessTemplate
+# 2. Create Project with said Process Template ID and Version Control type ($VersionControl)
+#  2.1 If Project already exists, skip to step 5 with the Project ID
 # 3. Monitor project creation via the Operations API
 # 4. Once created, retrieve the project ID by name
-# 5. Update the project's agent pool permissions for:
-#    - Build Administrator
-#    - Release Administrator
-#    - Project Administrator
+# 5. Update the project's agent pool permissions for all roles to the
+#       specified permissions ($PoolDefaultRole)
 # ==========================================================
 # Known Limitations:
 #   - ADO API does not support filtering within the request
 #     - filtering must be done after the response is received
 # ==========================================================
 
-# TODO: Parameterize Input Parameters
-$Organization = "https://dev.azure.com/jinle"
-$ProjectName = "rest-poc-delme"
-$PersonalAccessToken = $Env:PAT
-$PoolDefaultRole = "User"
-$ProcessTemplateName = "Agile"
-$DefaultRolesToUpdate = @(
-  "Build Administrator",
-  "Release Administrator",
-  "Project Administrator"
+[CmdLetBinding()]
+Param(
+    [Parameter(Mandatory=$true)]
+    [string] $Organization, # = "https://dev.azure.com/my-ado-org",
+    [Parameter(Mandatory=$true)]
+    [string] $ProjectName, # = "poc-test-delme2",
+    [Parameter()]
+    [string] $ProjectDescription,
+    [ValidateSet("Git","TFVC")]
+    [string] $VersionControl = "Git",
+    [Parameter()]
+    [string] $ProcessTemplate = "Scrum",
+    [Parameter()]
+    [string] $PoolDefaultRole = "User"
 )
 
+$PersonalAccessToken = $Env:PAT
 
+Set-StrictMode -Version Latest
+Set-Variable SerializationDepth -Option Constant -Value 100 -ErrorAction SilentlyContinue
 
 Function Invoke-DevOpsAPI
 {
@@ -39,7 +46,7 @@ Param(
     [parameter(Position=0, Mandatory=$true)]
     [string] $Uri,
     [Parameter(Position=1)]
-    [string] [ValidateSet('Default','Get', 'Post')]
+    [string] [ValidateSet('Default','Get', 'Post', 'Put')]
     $Method = 'Get',
     [parameter(Position=2, Mandatory=$true)]
     [string] $PersonalAccessToken,
@@ -78,32 +85,217 @@ Param(
         Write-Error "$($_.Exception.Message)"
         
     }
-
 }
 
+Function GET-ProcessTemplate {
+    Param (
+        [string] $Organization,    
+        [string] $ProcessTemplate,
+        [string] $PersonalAccessToken
+    )
+    # Get a list of process template for a given Organization/Collection
+    [string] $Uri = "$Organization/_apis/process/processes"
+    $result = Invoke-DevOpsAPI -Uri $Uri -PersonalAccessToken $PersonalAccessToken
+    if (($null -eq $result) -or ($result.Count -eq 0)) {
+        return $null
+    }
+    # Loop through the process templates and check by Name
+    foreach ($process in $result.Value) {
+        if ($process.name -eq $ProcessTemplate) {
+            return $process
+        }
+    }
+    # Template not found
+    return $null
+}
 
+Function GET-ADOProject {
+    Param(
+        [string] $Organization,    
+        [string] $ProjectName,
+        [string] $PersonalAccessToken
+    )
+    # Get a list of projects for a given Organization/Collection
+    [string] $Uri = "$Organization/_apis/projects?api-version=6.1-preview.4"
+    $result = Invoke-DevOpsAPI -Uri $Uri -PersonalAccessToken $PersonalAccessToken
+    if (($null -eq $result) -or ($result.Count -eq 0)) {
+        # Write-Host "Could not find projects within org"
+        return $null
+    }
+    # Loop through the projects and check by Name
+    foreach ($project in $result.Value) {
+        if ($project.name -eq $ProjectName) {
+            # Write-Host "Found Project: $ProjectName"
+            return $project
+        }
+    }
+    # Project not found
+    # Write-Host "Could not find projects within orgs"
 
+    return $null
+}
+
+Function New-ADOProject {
+    # https://dev.azure.com/{{organization}}/_apis/projects?api-version=6.0
+    Param(
+        [string] $Organization,    
+        [string] $ProjectName,
+        [string] $ProjectDescription,
+        [string] $VersionControl,
+        [string] $TemplateId,
+        [string] $PersonalAccessToken
+    )
+    [string] $Uri = "$Organization/_apis/projects?api-version=6.0"
+    [hashtable] $body = @{
+        "name" = "$ProjectName"
+        "capabilities" = @{ "versioncontrol" = @{ "sourceControlType" = $VersionControl}
+                            "processTemplate" = @{ "templateTypeId" = $TemplateId }
+        }
+        "visibility" = "private"
+    }
+    if (![string]::IsNullOrEmpty($ProjectDescription)) {
+        $body += @{ "description"=$ProjectDescription}
+    }
+    else {
+        $body += @{"description"="Autogenerated by Scrpipt"}
+    }
+    return Invoke-DevOpsAPI -Uri $Uri -PersonalAccessToken $PersonalAccessToken `
+        -Method Post `
+        -Body (ConvertTo-Json $body -Depth $SerializationDepth) `
+        -ContentType 'application/json'
+}
+
+Function Get-ProjectSecurityRoles {
+    #   https://dev.azure.com/{{organization}}/_apis/securityroles/scopes/distributedtask.globalagentqueuerole/roleassignments/resources/{{projectId}}?api-version=7.1-preview.1
+    Param (
+        [string] $Organization,    
+        [string] $ProjectId,
+        [string] $PersonalAccessToken
+    )
+    [string] $Uri = "$Organization/_apis/securityroles/scopes/distributedtask.globalagentqueuerole/roleassignments/resources/$($ProjectId)?api-version=7.1-preview.1"
+
+    return Invoke-DevOpsAPI -Uri $Uri -PersonalAccessToken $PersonalAccessToken
+}
+
+Function Get-Operation {
+    Param (
+        [string] $Organization,    
+        [string] $OperationId,
+        [string] $PersonalAccessToken
+    )
+    [string] $Uri = "$Organization/_apis/operations/$OperationId"
+    return Invoke-DevOpsAPI -Uri $Uri -PersonalAccessToken $PersonalAccessToken
+}
+
+Function PUT-AgentPoolRole {
+    #https://dev.azure.com/{{organization}}/_apis/securityroles/scopes/distributedtask.globalagentqueuerole/roleassignments/resources/{{roleId}}?api-version=7.1-preview.1
+    Param(
+        [string] $Organization,    
+        [string] $ProjectId,    
+        [array] $RoleIds,
+        [string] $RoleToAssign,
+        [string] $PersonalAccessToken
+    )
+    [string] $Uri = "$Organization/_apis/securityroles/scopes/distributedtask.globalagentqueuerole/roleassignments/resources/$($ProjectId)?api-version=7.1-preview.1"
+    
+    $body = @()
+
+    foreach ($role in $RoleIds) {
+        $body += @{
+            userId = $role
+            roleName = $RoleToAssign
+        }
+    }
+    
+    return Invoke-DevOpsAPI -Uri $Uri -PersonalAccessToken $PersonalAccessToken `
+        -Method Put `
+        -Body (ConvertTo-Json $body -Depth $SerializationDepth) `
+        -ContentType 'application/json'
+}
 # ==========================================================
 # 1. Retrieve Process Template ID by Name
 # ==========================================================
-Invoke-RestMethod -Uri "$Organization/_apis/projects?api-version=7.0" -Method get -Headers @{Authorization='Basic ' + [Convert]::ToBase64string([Text.Encoding]::ASCII.GetBytes("$PersonalAccessToken"))}
+# GET-ADOProject -Organization $Organization -ProjectName 'JinLeeSandbox' -PersonalAccessToken $PersonalAccessToken
+# Invoke-RestMethod -Uri "$Organization/_apis/projects?api-version=7.0" -Method get -Headers @{Authorization='Basic ' + [Convert]::ToBase64string([Text.Encoding]::ASCII.GetBytes("$PersonalAccessToken"))}
 
+# Find Process Template by Name
+$template = GET-ProcessTemplate -Organization $Organization -ProcessTemplate $ProcessTemplate -PersonalAccessToken $PersonalAccessToken
+if ($null -eq $template) {
+    Write-Host -ForegroundColor Red "Template '$ProcessTemplate' not found in '$Organization'"
+    exit 1
+}
+# ==========================================================
+# 2. If Project not exists, create Project with said Process Template ID
+#      Otherwise, retrieve Project ID
+# ==========================================================
+# Check if Projects already exists
+$project = GET-ADOProject -Organization $Organization -ProjectName $ProjectName -PersonalAccessToken $PersonalAccessToken
+
+if ($null -eq (GET-ADOProject -Organization $Organization -ProjectName $ProjectName -PersonalAccessToken $PersonalAccessToken)) {
+    Write-Host -ForegroundColor Yellow "Project '$ProjectName' does not exist in '$Organization'"
+    Write-Host "Creating Project '$ProjectName'" -NoNewline -ForegroundColor Yellow
+    $result = New-ADOProject `
+                -Organization $Organization `
+                -ProjectName $ProjectName -ProjectDescription $ProjectDescription `
+                -VersionControl $VersionControl `
+                -TemplateId $template.id `
+                -PersonalAccessToken $PersonalAccessToken
+        
+    # ==========================================================
+    # 3. Monitor project creation via the Operations API
+    # ==========================================================
+    # Get Operation results
+    $operation = Get-Operation -Organization $Organization -OperationId $result.id -PersonalAccessToken $PersonalAccessToken
+    if ($result.status -notin 'queued', 'succeeded', 'notSet')
+    {
+        Write-Host ""
+        Write-Host -ForegroundColor Red "Project creation faild! Status: '$($result.status)'"
+        Write-Host -ForegroundColor Red $operation.resultMessage
+        exit 3
+    }
+
+    # Wait for Project creation to finish
+    Do {
+        Write-Host "." -ForegroundColor Yellow -NoNewline
+        Start-Sleep -Seconds 1 
+        $operation = Get-Operation -Organization $Organization -OperationId $result.id -PersonalAccessToken $PersonalAccessToken
+    } while($operation.status -eq "inProgress")
+    # Write result of Operation
+    switch ($operation.status) {
+        "succeeded" { Write-Host ""; Write-Host -ForegroundColor Green "Project Creation Succeeded!"  }
+        Default { Write-Host -ForegroundColor Red "Project creation failed! $($operation.resultMessage)" exit 3}
+    }
+    $project = GET-ADOProject -Organization $Organization -ProjectName $ProjectName -PersonalAccessToken $PersonalAccessToken
+}
 
 # ==========================================================
-# 2. Create Project with said Process Template ID
-# ==========================================================
-
-# ==========================================================
-# 3. Monitor project creation via the Operations API
-# ==========================================================
-
-# ==========================================================
-# 4. Once created, retrieve the project ID by name
-# ==========================================================
-
-# ==========================================================
-# 5. Update the project's agent pool permissions for:
+# 4. Update the project's agent pool permissions for:
 #    - Build Administrator
 #    - Release Administrator
 #    - Project Administrator
 # ==========================================================
+# Get the current assigned project-scoped roles for the agent pools
+$roles = Get-ProjectSecurityRoles -Organization $Organization -ProjectId $project.id -PersonalAccessToken $PersonalAccessToken
+if ($null -eq $roles) {
+    Write-Host -ForegroundColor Red "Failed to retrieve project-scoped roles for agent pools"
+    exit 4
+}
+
+$roleIds = @()
+foreach ($role in $roles.value) {
+    $roleIds += $role.identity.id
+}
+
+$updatedRoles = PUT-AgentPoolRole -Organization $Organization -RoleIds $roleIds -ProjectId $project.id -RoleToAssign $PoolDefaultRole -PersonalAccessToken $PersonalAccessToken
+if ($null -eq $updatedRoles) {
+    Write-Host -ForegroundColor Red "Failed to update project agent pool permissions"
+    exit 4
+}
+
+foreach ($updatedRole in $updatedRoles.value) {
+    Write-Host ""
+    Write-Host -ForegroundColor Yellow "Updated Roles:"
+    $updatedRole | ConvertTo-Json -Depth $SerializationDepth | Write-Host -ForegroundColor Yellow -NoNewline
+}
+
+Write-Host ""; Write-Host -ForegroundColor Green "Project Agent Pool Role Assignment Succeeded!"; exit 0
